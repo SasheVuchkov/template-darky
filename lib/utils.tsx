13b368d13ba8cb936577db35614423c4a8a0fa24
Sashe Vuchkov <sashevuchkov@gmail.com>
2022-10-12 00:11:58 +0300

    Added new script

--- lib/utils.tsx
+++ lib/utils.tsx
@@ -1,4 +1,6 @@
 import {monthNames} from './constants';
 
+export const isDev = process.env.NODE_ENV !== 'production';
 export const formatDate = (date: Date) => `${monthNames[date.getMonth()]} ${date.getDate()}, ${date.getFullYear()}`;
 export const getCategorySlug = (cat: string) => cat.replaceAll(' ', '-').replaceAll(/[^-a-z0-9_]+/ig, '').toLowerCase();
+
